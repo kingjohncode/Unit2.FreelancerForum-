e0ee7b39e9b492d71fff15f66d6f3d06154a5b01
kingjohncode <jkng237@gmail.com>
2024-08-22 20:22:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
+//Create html tags and add text 
+const h1 = document.createElement("h1");
+h1.textContent = "Freelancer Forum";
 
+const p = document.createElement("p");
+p.textContent = "The average starting price is $30";
+const h2 = document.createElement("h2");
+h2.textContent = "Available Freelancers";
+
+//
+
+
+
+//Create a table
+
+const div = document.querySelector(section);
+
+document.getElementById(section);
+
+
+const table = document.createElement(table);
+
+const headers = ["Name", "Occupation", "Starting Price"];
+
+div.appendChild(table)
+
+const th1 = document.createElement(th1);
+th1.textContent = (Name);
+tr.appendChild(th1);
+
+const th2 = document.createElement(th2);
+th2.textContent = (Occupation);
+tr.appendChild(th2);
+
+const th3 = document.createElement(th3);
+th3.textContent = (StartingPrice);
+tr.appendChild(th3);
+
+const tr1 = document.createElement(tr1);
+const tr2 = document.createElement(tr2);
+const tr3 = document.createElement(tr3);
+
+const td1 = document.createElement(td1);
+const td2 = document.createElement(td2);
+const td3 = document.createElement(td3);
+
+
+
+
+//Create data arrays
+const Name = ["Alice", "Bob", "Carol"];
+const Occupation = ["writer", "Teacher", "Programmer"];
+const Price = [$30, $50, $70];
